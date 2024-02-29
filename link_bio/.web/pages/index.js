21f35e522d3dfe45c0833326b0bdb9ea21495ef7
bereamanuel <mbaberearellano@gmail.com>
2024-02-29 16:28:03 +0100

    HolaReflex!

---
+++ link_bio/.web/pages/index.js
@@ -0,0 +1,59 @@
+/** @jsxImportSource @emotion/react */
+
+
+import { Fragment, useContext } from "react"
+import { EventLoopContext } from "/utils/context"
+import { Event, getBackendURL, isTrue } from "/utils/state"
+import { Dialog as RadixThemesDialog, Text as RadixThemesText } from "@radix-ui/themes"
+import env from "/env.json"
+import NextHead from "next/head"
+
+
+
+export function Fragment_1762bb90abdb81b879b2a22edbbe01a1 () {
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+
+  return (
+    <Fragment>
+  {isTrue(connectError !== null) ? (
+  <Fragment>
+  <RadixThemesDialog.Root open={connectError !== null}>
+  <RadixThemesDialog.Content>
+  <RadixThemesDialog.Title>
+  {`Connection Error`}
+</RadixThemesDialog.Title>
+  <RadixThemesText as={`p`}>
+  {`Cannot connect to server: `}
+  {(connectError !== null) ? connectError.message : ''}
+  {`. Check if server is reachable at `}
+  {getBackendURL(env.EVENT).href}
+</RadixThemesText>
+</RadixThemesDialog.Content>
+</RadixThemesDialog.Root>
+</Fragment>
+) : (
+  <Fragment/>
+)}
+</Fragment>
+  )
+}
+
+export default function Component() {
+
+  return (
+    <Fragment>
+  <Fragment_1762bb90abdb81b879b2a22edbbe01a1/>
+  <RadixThemesText as={`p`} css={{"color": "blue"}}>
+  {`Hola Reflex!`}
+</RadixThemesText>
+  <NextHead>
+  <title>
+  {`Reflex App`}
+</title>
+  <meta content={`A Reflex app.`} name={`description`}/>
+  <meta content={`favicon.ico`} property={`og:image`}/>
+</NextHead>
+</Fragment>
+  )
+}
--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -1,5 +1,6 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="link_bio",
+    app_name="link_bio"
 )
+
